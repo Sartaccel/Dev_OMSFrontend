a2077c283c7527dfca0d7da222de7ce8f12f5bbe
Asmism <asmism2020@gmail.com>
2026-02-10 13:19:17 +0530

    User

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,8 @@ import Dashboard from "../pages/Dashboard";
 import Tasks from "../pages/Tasks";
 import Newtasks from "../pages/Newtasks";
 import Clients from "../pages/Clients";
+import Users from "../pages/User/UserList";
+import NewUser from "../pages/User/NewUser";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
@@ -19,6 +21,9 @@ export default function AppRouter() {
           <Route path="tasks" element={<Tasks />} />
           <Route path="newtasks" element={<Newtasks />} />
           <Route path="clients" element={<Clients />} />
+           <Route path="users" element={<Users />} />
+            <Route path="users/add" element={<NewUser />} />
+            <Route path="users/edit/:id" element={<NewUser />} />
         </Route>
       </Routes>
     </BrowserRouter>
